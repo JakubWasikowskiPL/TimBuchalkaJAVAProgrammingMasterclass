802a0359f0765ba30ed9047b193799cee4dab2d3
JakubWasikowski <jakubwasikowskipriv@gmail.com>
2021-10-24 19:50:10 +0200

    Update of DB progress 24.10.2021

---
+++ Databases/TheMusicSQLiteDatabase/src/com/jakubwasikowski/Main.java
@@ -0,0 +1,16 @@
+package com.jakubwasikowski;
+
+import com.jakubwasikowski.model.Datasource;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Datasource datasource = new Datasource();
+        if(!datasource.open()) {
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        datasource.close();
+    }
+}
---
+++ JavaNetworking/MultiThreadingAndTimeouts/EchoClient/src/com/jakubwasikowski/Main.java
@@ -0,0 +1,46 @@
+package com.jakubwasikowski;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map<String,String>map = new HashMap<>();
+
+        try (Socket socket = new Socket("localhost", 5000)) {
+
+            socket.setSoTimeout(2000);
+            BufferedReader echoes = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
+
+            Scanner scanner = new Scanner(System.in);
+            String echoString;
+            String response;
+
+            do {
+                System.out.println("Enter string to be echoed: ");
+                echoString = scanner.nextLine();
+
+                stringToEcho.println(echoString);
+                if (!echoString.equals("exit")) {
+                    response = echoes.readLine();
+                    System.out.println(response);
+                }
+            } while (!echoString.equals("exit"));
+        } catch (SocketTimeoutException e) {
+            System.out.println("The socket timed out");
+        } catch (IOException e) {
+            System.out.println("Client Error: " + e.getMessage());
+
+        }
+    }
+}
